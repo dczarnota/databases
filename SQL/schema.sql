5a347d94154d3fbb4af65a82a40a24ddf1da06bf
dczarnota <dczarno@gmail.com>
2014-08-15 16:57:39 -0700

    Created mysql database and tables.

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -2,9 +2,22 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  user_id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL UNIQUE
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id INT NOT NULL PRIMARY KEY,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  message VARCHAR(100) NOT NULL,
+  roomname VARCHAR(100) NOT NULL
 );
+
 
 /* Create other tables and define schemas for them here! */
 
@@ -14,7 +27,3 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
-
-
-
-
